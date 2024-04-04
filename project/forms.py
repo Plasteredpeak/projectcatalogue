ac771b5217b32967b6be81b0cb721fc6d999024c
Ali Hamza <hamza120025@gmail.com>
2024-04-05 00:03:23 +0100

    updated gitignore

--- project/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from .models import ProjectFile
-
-
-class ProjectFileForm(ModelForm):
-    class Meta:
-        model = ProjectFile
-        fields = ('name', 'attachment',)
