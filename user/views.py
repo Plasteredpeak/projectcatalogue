75effab0263f25834d4a979197dbcdb3020bfe4c
Ali Hamza <hamza120025@gmail.com>
2024-04-05 16:32:18 +0100

    changed login redirect

--- user/views.py
+++ user/views.py
@@ -18,7 +18,7 @@ def login(request):
 
             if user is not None:
                 auth_login(request, user)
-                return redirect('/')
+                return redirect('/projects')
     else:
         form = LoginForm()
 
